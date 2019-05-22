04331ea1b126b1627967762ae373e07b5935669f
Joy Lin <vvlnote@hotmail.com>
2019-05-22 23:07:10 +0000

    Automatically backed up by Learn

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  resources :songs 
+  resources :songs, only: [:show, :create, :new, :edit, :update]
   resources :genres
   resources :artists
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
