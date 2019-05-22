6e0a5640cc99cb3ff51da8c2462f32cb573146cd
Joy Lin <vvlnote@hotmail.com>
2019-05-22 23:59:24 +0000

    Automatically backed up by Learn

--- app/controllers/songs_controller.rb
+++ app/controllers/songs_controller.rb
@@ -10,11 +10,14 @@ class SongsController < ApplicationController
   end
   
   def edit
-    
+    @song = Song.find(params[:id])
   end
   
   def update
-    
+    @song = Song.find(params[:id])
+    @song.update(post_params(:name, :artist_id, :genre_id))
+    @song.save
+    redirect_to song_path(@song)
   end
   
   def show
