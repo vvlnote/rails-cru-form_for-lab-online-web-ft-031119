03e87a48e83278c3eca7adaa4ae0d39b387b1c76
Joy Lin <vvlnote@hotmail.com>
2019-05-22 23:51:06 +0000

    Automatically backed up by Learn

--- app/controllers/songs_controller.rb
+++ app/controllers/songs_controller.rb
@@ -5,7 +5,7 @@ class SongsController < ApplicationController
   end
   
   def create
-    @song = Song.create(post_params(:name :artist_id, :genre_id))
+    @song = Song.create(post_params(:name, :artist_id, :genre_id))
     redirect_to song_path(@song)
   end
   
