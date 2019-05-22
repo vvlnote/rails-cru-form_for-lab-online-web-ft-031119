f201d9d6e1a044e37eeb4261be7ccb0f4be3dba7
Joy Lin <vvlnote@hotmail.com>
2019-05-22 23:23:53 +0000

    Automatically backed up by Learn

--- app/controllers/artists_controller.rb
+++ app/controllers/artists_controller.rb
@@ -6,7 +6,7 @@ class ArtistsController < ApplicationController
   
   def create
     @artist = Artist.create(post_params(:name, :bio))
-    redirect_to
+    redirect_to artist_path(@artist)
   end
   
   def edit
