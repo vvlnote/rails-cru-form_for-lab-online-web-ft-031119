1e033c61bfd19ef63782c7276eda1359990bb855
Joy Lin <vvlnote@hotmail.com>
2019-05-22 23:03:36 +0000

    Automatically backed up by Learn

--- app/controllers/artists_controller.rb
+++ app/controllers/artists_controller.rb
@@ -1,2 +1,22 @@
 class ArtistsController < ApplicationController
+  
+  def new
+  
+  end
+  
+  def create
+    
+  end
+  
+  def edit
+    
+  end
+  
+  def update
+    
+  end
+  
+  def show
+    
+  end
 end
